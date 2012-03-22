065d61d6bea3340199d7adad3db3b6c3146adeed
Kevin Mees <kev.mees@gmail.com>
2012-03-22 23:22:00 +0100

    Rakefile to the rescue

--- lib/ut.rb
+++ lib/ut.rb
@@ -1,3 +1,7 @@
+# # Unicodetiles.rb
+#
+# Ruby Port of [Unicodetiles.js](https://github.com/tapio/unicodetiles.js)
+
 $LOAD_PATH << './lib'
 
 require 'rubygems'
@@ -10,3 +14,7 @@ require 'ut/viewport'
 require 'ut/engine'
 require 'ut/font_renderer'
 require 'ut/version'
+
+module UT
+
+end
